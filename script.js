3592f1ec5a845dfc84991fcdc09415dbb0a9f50f
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-06-02 01:17:20 -0500

    declare formSubmitButton variable and assign id that's that associated to the submit button in html. Added event listner method and included name of the function that will be called when button is clicked

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Write your JavaScript code here!
 
+
+let formSubmitButton = document.getElementById("formSubmit");
+formSubmitButton.addEventListener("click", formClicked);
+
+
+
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
