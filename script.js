f1721cc535fa716056bf585ca2a2f1f1f7feb9c7
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-06-12 01:32:19 -0500

    Made list in launch status visible if input satisfy input submission requiremts. Will show user's input to pilot and copilot name as strings in launch status box. H2 will change to to show that shuttle is not ready if fuel level and cargo mass requirement is not met, the text color would change to red as well

--- script.js
+++ script.js
@@ -7,19 +7,40 @@ window.addEventListener("load", function(){
    let coPilotInput = document.getElementById("copilotName");
    let fuelLevelInput = document.getElementById("fuelLevel");
    let cargoMassInput = document.getElementById("cargoMass");
+   let launchStatus = document.getElementById("launchStatus");
+   let faultyItems = document.getElementById("faultyItems");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let copilotStatus = document.getElementById("copilotStatus");
+   let fuelStatus = document.getElementById("fuelStatus");
+   let cargoStatus = document.getElementById("cargoStatus");
    
    function formSubmit(event){
+      event.preventDefault();
       if (pilotInput.value === "" || coPilotInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === ""){
-         event.preventDefault();
          alert("All fields are required!");
       }else if (isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)){
-         event.preventDefault();
          alert("Fuel level and cargo mass must be numbers!");
+      }else{
+         faultyItems.style.visibility = "visible";
+         pilotStatus.innerHTML = `${pilotInput.value} Ready`;
+         copilotStatus.innerHTML = `${coPilotInput.value} Ready`;
       }
+      
+      if (fuelLevelInput.value < 10000 || cargoMassInput.value > 10000){
+         launchStatus.innerHTML = "Shuttle not ready for launch.";
+         launchStatus.style.color ="red";
+         }if (cargoMassInput.value > 10000){
+            cargoStatus.innerHTML =  "There is too much mass for the shuttle to take off."
+         }else if (fuelLevelInput.value < 10000)
+            fuelStatus.innerHTML =  "There is not enough fuel for the journey.";
+   
+      
+      //pilotStatus.innerHTML = `${pilotInput.value} Ready`
    }
 
+
+
    form.addEventListener("submit", formSubmit);
-   
 });
 
 
